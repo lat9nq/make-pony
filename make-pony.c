ecfe5ea190ca8675b66fc51a1f5fe9b541232d37
Louis Thomas <lat9nq@virginia.edu>
2017-09-24 23:23:49 -0400

    -m option now conveys style

--- make-pony.c
+++ make-pony.c
@@ -255,6 +255,9 @@ int main(int argc, char * argv[]) {
 			if (male) {
 				sprintf(temp, "%s -g", temp);
 			}
+			if (key != -1) {
+				sprintf(temp, "%s -d%d", temp, key);
+			}
 			sprintf(cmd, "%s -s%ld %s %s" , argv[0], seed+i, (verbose) ? "-v" : "", temp);
 			if (verbose) {
 				fprintf(stderr, "%s\n", cmd);
