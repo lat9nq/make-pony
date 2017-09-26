f566c7701e81070469f64b5a73f575dbb958eee6
Louis Thomas <lat9nq@virginia.edu>
2017-09-25 22:17:41 -0400

    Fixed analyzer to not output color keys incorrectly

--- target.h
+++ target.h
@@ -8,10 +8,12 @@
 #define VAL 	3
 #define BOOL	4
 
-#define TARGETCOUNT 778
+#define TARGETCOUNT 640
+#define TARGETCOUNTSIMPLE 70
 #define PUSHTARGET(R,X,T)	sprintf((R)[target_at], "%c%s", (T), (X)); target_at++
 int target_at;
 
 void importTargets(char ** target);
+void importTargetsSimple(char ** target);
 
 #endif
