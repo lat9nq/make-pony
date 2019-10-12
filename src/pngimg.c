e5d5bfbd35485bd3807fce2fb98e2c42ba9adaeb
lat9nq <lat9nq@virginia.edu>
2019-10-12 18:18:40 -0400

    Maybe fixed PNGIMG

--- src/pngimg.c
+++ src/pngimg.c
@@ -106,26 +106,38 @@ int pngimg_write(PNGIMG * img, FILE * f) {
 		return -1;
 	}
 	png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (!png_ptr) {
+		fprintf(stderr, "error: Write struct creation failed.\n");
+		return -1;
+	}
 	png_infop info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr) {
+		fprintf(stderr, "error: Info struct creation failed.\n");
+		return -1;
+	}
 	if (setjmp(png_jmpbuf(png_ptr))) {
+		png_destroy_write_struct(&png_ptr, &info_ptr);
 		fprintf(stderr,"error: failure on write initialize\n");
 		return -2;
 	}
 	png_init_io(png_ptr, f);
 	if (setjmp(png_jmpbuf(png_ptr))) {
+		png_destroy_write_struct(&png_ptr, &info_ptr);
 		fprintf(stderr,"error: failure writing header\n");
 		return -3;
 	}
 	png_set_IHDR(png_ptr, info_ptr, img->width, img->height,
-		8, 6, PNG_INTERLACE_NONE,
-		PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+		8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
+		PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 	png_write_info(png_ptr, info_ptr);
 	if (setjmp(png_jmpbuf(png_ptr))) {
+		png_destroy_write_struct(&png_ptr, &info_ptr);
 		fprintf(stderr,"error: failure writing bytes\n");
 		return -4;
 	}
 	png_write_image(png_ptr, rows);
 	if (setjmp(png_jmpbuf(png_ptr))) {
+		png_destroy_write_struct(&png_ptr, &info_ptr);
 		fprintf(stderr,"error: failure finishing image\n");
 		return -5;
 	}
